a0bc0b8902d90302fdcdd47a24c76d76826a3528
rjprattes <rprates55@gmail.com>
2021-10-05 21:28:14 -0300

    Teste2

---
+++ Calculadora/src/Calculadora.java
@@ -0,0 +1,12 @@
+public class Calculadora{
+
+	public static void main(String[] args) {
+		float pi = 3.14f;
+		
+		float raio = 5.3f;
+		
+		float area = raio * raio * pi;
+		
+		System.out.println("Resultado: " + area);
+	}
+}
