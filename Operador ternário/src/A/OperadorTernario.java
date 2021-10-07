3d8c59866f69354e21939d247b2d14f397639dfe
rjprattes <rprates55@gmail.com>
2021-10-07 19:51:42 -0300

    Operador Ternário

---
+++ Operador ternário/src/A/OperadorTernario.java
@@ -0,0 +1,16 @@
+package A;
+
+import java.util.Scanner;
+
+public class OperadorTernario {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Digite sua idade: ");
+		int idade = entrada.nextInt();
+		
+		String indicacao = (idade >= 18) ? "Adulto" : "Criança";
+			
+		System.out.println("Resultado: " + indicacao);
+	}
+}
